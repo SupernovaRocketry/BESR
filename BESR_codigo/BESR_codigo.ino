03396fa5dba76473f460f38d3b3047c2cfc74228
Pedro Cardoso <pedro.cardoso@engenharia.ufjf.br>
2019-10-27 17:25:26 -0300

    v

--- BESR_codigo/BESR_codigo.ino
+++ BESR_codigo/BESR_codigo.ino
@@ -1,48 +1,50 @@
 
 
 /*
-  _____                                                        ______               _           _                
-/  ___|                                                       | ___ \             | |         | |               
-\ `--.  _   _  _ __    ___  _ __  _ __    ___  __   __  __ _  | |_/ /  ___    ___ | | __  ___ | |_  _ __  _   _ 
- `--. \| | | || '_ \  / _ \| '__|| '_ \  / _ \ \ \ / / / _` | |    /  / _ \  / __|| |/ / / _ \| __|| '__|| | | |
-/\__/ /| |_| || |_) ||  __/| |   | | | || (_) | \ V / | (_| | | |\ \ | (_) || (__ |   < |  __/| |_ | |   | |_| |
-\____/  \__,_|| .__/  \___||_|   |_| |_| \___/   \_/   \__,_| \_| \_| \___/  \___||_|\_\ \___| \__||_|    \__, |
+  _____                                                        ______               _           _
+  /  ___|                                                       | ___ \             | |         | |
+  \ `--.  _   _  _ __    ___  _ __  _ __    ___  __   __  __ _  | |_/ /  ___    ___ | | __  ___ | |_  _ __  _   _
+  `--. \| | | || '_ \  / _ \| '__|| '_ \  / _ \ \ \ / / / _` | |    /  / _ \  / __|| |/ / / _ \| __|| '__|| | | |
+  /\__/ /| |_| || |_) ||  __/| |   | | | || (_) | \ V / | (_| | | |\ \ | (_) || (__ |   < |  __/| |_ | |   | |_| |
+  \____/  \__,_|| .__/  \___||_|   |_| |_| \___/   \_/   \__,_| \_| \_| \___/  \___||_|\_\ \___| \__||_|    \__, |
               | |                                                                                          __/ |
-              |_|                                                                                         |___/ 
+              |_|                                                                                         |___/
 
-BESR: Banco Estático Supernova Rocketry
+  BESR: Banco Estático Supernova Rocketry
 
-Toda a parte editável pelo usuário está em //config
-Conexões:
+  Toda a parte editável pelo usuário está em //config
+  Conexões:
 
- */
+*/
 
 //Bibliotecas
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <SD.h>
-#include "HX711.h" 
+//#include "SdFat.h"
+//SdFat SD;
+#include "HX711.h"
 
 //Configurações
-#define TAXA_ATUALIZ 50
+#define TAXA_ATUALIZ 5
 #define TAXA_TELA 100
 #define DEBOUNCE_MS 500
 #define CALIB_MS 5000
-#define PESO_ESTIMADO 250.0
+#define PESO_ESTIMADO -250.0
 #define DELTA_RUIDO 1500
 #define HX_DELAY_INICIO 2000
 //#define DEBUG_SERIAL
 
-const int TELA_RS = 2;
-const int TELA_EN = 3;
-const int TELA_D4 = 4;
-const int TELA_D5 = 5;
-const int TELA_D6 = 6;
-const int TELA_D7 = 7;
+//const int TELA_RS = 2;
+//const int TELA_EN = 3;
+//const int TELA_D4 = 4;
+//const int TELA_D5 = 5;
+//const int TELA_D6 = 6;
+//const int TELA_D7 = 7;
 
 const int PINO_SD_CS = 8;
 
-#define PINO_BOTAO A5
+#define PINO_BOTAO 9
 #define TAMANHO_BUFFER 512
 
 //Variáveis do código, não mexer
@@ -52,10 +54,17 @@ char  submenu = 'a';
 float   peso = 0;
 float   maiorPeso = 0;
 
-int   millisAtual, millisAnterior;
-int   millisTela;
-int   millisExpira;
+unsigned long   millisAtual, millisAnterior;
+unsigned long   millisTela;
+unsigned long   millisExpira;
+unsigned long   millisIniciou;
+unsigned long   millisDado;
+unsigned long  millis1s;
+
+
+int   nDado = 0;
 int   telaMudou = 0;
+int   novoarquivo;
 
 int estadoBotao;
 int ultimoEstadoBotao;
@@ -72,7 +81,7 @@ const int   HX_CLK = A1;
 const float FATOR_CALIB = 2.00;
 HX711 celulaCarga(HX_DOUT, HX_CLK);
 
-LiquidCrystal tela(TELA_RS, TELA_EN, TELA_D4, TELA_D5, TELA_D6, TELA_D7);
+LiquidCrystal_I2C tela(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int debounceBotao();
 void medir();
@@ -84,92 +93,92 @@ void setup() {
   //Setup do display e inicialização
 
   pinMode(PINO_BOTAO, INPUT);
-  tela.begin(16,2);
+  tela.begin(16, 2);
 
-  #ifdef DEBUG_SERIAL 
+#ifdef DEBUG_SERIAL
   Serial.begin(115200);
-  #endif
-  
+#endif
+
 }
 
 void loop() {
-  
+
   millisAtual = millis();
 
   //Loop principal do banco estático
-  
-  if(millisAtual - millisAnterior > TAXA_ATUALIZ){
-      
-      #ifdef DEBUG_SERIAL 
-      Serial.print("Menu: ");
-      Serial.print(menu);
-      Serial.print(" Submenu: ");
-      Serial.println(submenu);
-      #endif
-        
-      switch (menu){
-        case 'i':
+
+  if (millisAtual - millisAnterior > TAXA_ATUALIZ) {
+
+#ifdef DEBUG_SERIAL
+    Serial.print("Menu: ");
+    Serial.print(menu);
+    Serial.print(" Submenu: ");
+    Serial.println(submenu);
+#endif
+
+    switch (menu) {
+      case 'i':
         inicializa();
         break;
-    
-        case 'c':
+
+      case 'c':
         calibra();
         break;
-    
-        case 'e':
+
+      case 'e':
         espera();
         break;
-    
-        case 't':
+
+      case 't':
         trabalhando();
         break;
-        
-      }
 
-      millisAnterior = millisAtual;
+    }
+
+    millisAnterior = millisAtual;
   }
 
 
   //Atualizando a tela caso passado o tempo definido
-  if((millisAtual - millisTela > TAXA_TELA) && telaMudou ){
-  tela.clear();
-  telaMudou = 0;
-  millisTela = millisAtual;
+  if ((millisAtual - millisTela > TAXA_TELA) && telaMudou ) {
+    tela.clear();
+    telaMudou = 0;
+    millisTela = millisAtual;
   }
 }
 
 
 
-void inicializa(){
+void inicializa() {
 
-  switch(submenu){
+  switch (submenu) {
 
     case 'a':
-    tela.setCursor(0, 0);
-    tela.print("  Iniciando...  ");
-    
-    break;
+      tela.setCursor(0, 0);
+      tela.print("  Iniciando...  ");
+
+      break;
 
     case 'b':
-    tela.setCursor(0, 0);
-    tela.print("   Erro no SD   ");
-    
-    break;
+      tela.setCursor(0, 0);
+      tela.print("   Erro no SD   ");
+
+      break;
 
     case 'c':
-    tela.setCursor(0, 0);
-    tela.print("   Erro no HX   ");
-    
-    break;
+      tela.setCursor(0, 0);
+      tela.print("   Erro no HX   ");
+
+      break;
 
   }
 
   //inicializar o Cartão SD
-  if(!SD.begin(PINO_SD_CS)){
-  submenu = 'b';
-  telaMudou = 1;
+  if (!SD.begin(PINO_SD_CS)) {
+    submenu = 'b';
+    telaMudou = 1;
   }
-  else{
+  else {
 
     int n = 1;
     bool parar = false;
@@ -179,235 +188,260 @@ void inicializa(){
     {
       sprintf(nomeConcat, "BESR%d.txt", n);
       if (SD.exists(nomeConcat))
-          n++;
-          else
-            parar = true;
-      }
+        n++;
+      else
+        parar = true;
+    }
 
-  arquivoLog = SD.open(nomeConcat, FILE_WRITE);
+    arquivoLog = SD.open(nomeConcat, O_CREAT | O_WRITE);
   }
-  
+
   //inicializar o HX711
   //Setup da célula de carga, sua inicialização é feita posteriormente
-  
+
   delay(HX_DELAY_INICIO);
   celulaCarga.set_scale(FATOR_CALIB);
-  float zero = celulaCarga.get_units(100);
+  float zero = celulaCarga.get_units(50);
   celulaCarga.tare(zero);
 
-  if(celulaCarga.is_ready()){
-  submenu = 'c';
-  telaMudou = 1;
+  if (celulaCarga.is_ready()) {
+    submenu = 'c';
+    telaMudou = 1;
   }
 
-  if(submenu == 'a'){
-  menu = 'c';
-  submenu = 'a';
-  telaMudou = 1;
+  if (submenu == 'a') {
+    menu = 'c';
+    submenu = 'a';
+    telaMudou = 1;
   }
 }
 
-void calibra(){
+void calibra() {
 
-  switch(submenu){
+  switch (submenu) {
 
     case 'a':
-    tela.setCursor(0, 0);
-    tela.print("Calibrar o banco");
-    tela.setCursor(0, 1);
-    tela.print("   [Calibrar]   ");
+      tela.setCursor(0, 0);
+      tela.print("Calibrar o banco");
+      tela.setCursor(0, 1);
+      tela.print("   [Calibrar]   ");
 
-    if(debounceBotao()){
-    submenu = 'b';
-    telaMudou = 1;
-    }
-    
-    break;
+      if (debounceBotao()) {
+        submenu = 'b';
+        telaMudou = 1;
+      }
+
+      break;
 
     case 'b':
-    tela.setCursor(0, 0);
-    tela.print("Posicione o peso");
-    tela.setCursor(0, 1);
-    tela.print("      [OK]      ");
+      tela.setCursor(0, 0);
+      tela.print("Posicione o peso");
+      tela.setCursor(0, 1);
+      tela.print("      [OK]      ");
 
-    if(debounceBotao()){
-    submenu = 'c';
-    millisExpira = millisAtual;
-    telaMudou = 1;
-    }
-    break;
+      if (debounceBotao()) {
+        submenu = 'c';
+        millisExpira = millisAtual;
+        telaMudou = 1;
+      }
+      break;
 
     case 'c':
-    medir();
-    tela.setCursor(0, 0);
-    tela.print("Valor atual:");
-    tela.setCursor(0, 1);
-    tela.print(peso);
-    
-    if(millisAtual - millisExpira > CALIB_MS){
+      medir();
+      tela.setCursor(0, 0);
+      tela.print("Valor atual:");
+      tela.setCursor(0, 1);
+      tela.print(peso);
 
-      if(maiorPeso > PESO_ESTIMADO){
-      submenu = 'd';
-      telaMudou = 1;
+      if (millisAtual - millisExpira > CALIB_MS) {
+
+        if (maiorPeso > PESO_ESTIMADO) {
+          submenu = 'd';
+          telaMudou = 1;
+          maiorPeso = 0;
+        }
+
+        else {
+          submenu = 'e';
+          telaMudou = 1;
+        }
+
       }
 
-      else{
-      submenu = 'e';
-      telaMudou = 1;
-      }
-        
-    }
-    
-    break;
+      break;
 
     case 'd':
-    tela.setCursor(0, 0);
-    tela.print("Calib. concluida");
-    tela.setCursor(0, 1);
-    tela.print("      [OK]      ");
+      tela.setCursor(0, 0);
+      tela.print("Calib. concluida");
+      tela.setCursor(0, 1);
+      tela.print("      [OK]      ");
 
-    if(debounceBotao()){
-    submenu = 'a';
-    menu = 'e';
-    telaMudou = 1;
-    }
-    
-    break;
+      if (debounceBotao()) {
+        submenu = 'a';
+        menu = 'e';
+        telaMudou = 1;
+      }
+
+      break;
 
     case 'e':
-    tela.setCursor(0, 0);
-    tela.print("Erro! Verif cabo");
-    tela.setCursor(0, 1);
-    tela.print("    [Voltar]    ");
+      tela.setCursor(0, 0);
+      tela.print("Erro! Verif cabo");
+      tela.setCursor(0, 1);
+      tela.print("    [Voltar]    ");
 
-    if(debounceBotao()){
-    submenu = 'a';
-    telaMudou = 1;
-    }
+      if (debounceBotao()) {
+        submenu = 'a';
+        telaMudou = 1;
+      }
 
-    break;
-
-  }
+      break;
 
   }
-  
 
-void espera(){
+}
+
+
+void espera() {
 
   tela.setCursor(0, 0);
   tela.print("Pronto p/ medir");
   tela.setCursor(0, 1);
   tela.print("   [Iniciar]   ");
 
-  if(debounceBotao()){
+  if (debounceBotao()) {
     menu = 't';
     submenu = 'a';
     telaMudou = 1;
+    novoarquivo = 0;
+    millisIniciou = millis();
+    millis1s = millisIniciou;
   }
-  
+
 }
 
-void trabalhando(){
+void trabalhando() {
 
-  switch(submenu){
+  switch (submenu) {
 
     case 'a':
-    medir();
-    escrever();
-    
-    tela.setCursor(0, 0);
-    tela.print("A:      M:      ");
-    
-    tela.setCursor(3, 0);
-    tela.print(peso/1000);
+      medir();
+      escrever();
 
-    tela.setCursor(12, 0);
-    tela.print(maiorPeso/1000);
-    
-    
-    tela.setCursor(0, 1);
-    tela.print("   [Terminar]   ");
+      tela.setCursor(0, 0);
+      tela.print("A:      M:      ");
 
-    if(debounceBotao()){
-    submenu = 'b';
-    telaMudou = 1;
-    }
-    
-    break;
+      tela.setCursor(3, 0);
+      tela.print(peso / 1000);
+
+      tela.setCursor(12, 0);
+      tela.print(maiorPeso / 1000);
+
+
+      tela.setCursor(0, 1);
+      tela.print("   [Terminar]   ");
+
+      if (debounceBotao()) {
+        submenu = 'b';
+        telaMudou = 1;
+      }
+
+      break;
 
     case 'b':
-    tela.setCursor(0, 0);
-    tela.print("Salvo:");
-    tela.setCursor(7, 0);
-    tela.print(nomeConcat);
-    tela.setCursor(0, 1);
-    tela.print("      [OK]      ");
+      tela.setCursor(0, 0);
+      tela.print("Salvo:");
+      tela.setCursor(7, 0);
+      tela.print(nomeConcat);
+      tela.setCursor(0, 1);
+      tela.print("      [OK]      ");
 
-    finaliza();
+      finaliza();
 
-    if(debounceBotao()){
-    menu = 'e';
-    telaMudou = 1;
-    }
-    
-    break;
+      if (debounceBotao()) {
+        menu = 'e';
+        telaMudou = 1;
+      }
+
+      break;
 
   }
-  
+
 }
 
-void medir(){
+void medir() {
 
-    peso = celulaCarga.get_units();
+  nDado++;
+  peso = celulaCarga.get_units();
+  millisDado = millis() - millisIniciou;
 
-    if (peso < 0.00)
-      peso = 0.00;
-  
+  if (peso < 0.00)
+    peso = 0.00;
 
-    if (peso > maiorPeso)
+
+  if (peso > maiorPeso)
     maiorPeso = peso;
 
 }
 
-int debounceBotao(){
+int debounceBotao() {
 
-if(digitalRead(PINO_BOTAO)){
-  botaoApertado = millis();
+  if (digitalRead(PINO_BOTAO)) {
+    botaoApertado = millis();
 
-  if((botaoApertado - ultimoBotaoApertado )> DEBOUNCE_MS){
-    ultimoBotaoApertado = botaoApertado;
-    return 1;
-   }
-  else{
-    return 0;
-  }
+    if ((botaoApertado - ultimoBotaoApertado ) > DEBOUNCE_MS) {
+      ultimoBotaoApertado = botaoApertado;
+      return 1;
+    }
+    else {
+      return 0;
+    }
   }
   return 0;
 }
 
 
-void escrever(){
+void escrever() {
 
   stringDados = "";
-  stringDados += millisAtual;
-  stringDados += ",";
+  //stringDados += nDado;
+  //stringDados += "; ";
+  stringDados += millisDado;
+  stringDados += ";";
   stringDados += peso;
-  stringDados += ",";
-  stringDados += maiorPeso;
+  //stringDados += "; ";
+  //stringDados += maiorPeso;
   arquivoLog.println(stringDados);
-  arquivoLog.flush();
 
-  //stringDados += "\n";
-  
-  /* if(sizeof(stringDados) == TAMANHO_BUFFER){
-  arquivoLog.print(stringDados);
-  arquivoLog.flush();
-  stringDados = "";
-  } */
-
-  
+//  if (millis() - millis1s >= 1000) {
+//    arquivoLog.close();
+//    arquivoLog = SD.open(nomeConcat, FILE_WRITE);
+//    millis1s = millis();
+//  }
 }
 
-void finaliza(){
+void finaliza() {
   arquivoLog.close();
+//  if (!SD.begin(PINO_SD_CS)) {
+//    submenu = 'b';
+//    telaMudou = 1;
+//  }
+//  else {
+//
+//    bool parar = false;
+//
+//
+//    while (parar==false && novoarquivo==0)
+//    {
+//      sprintf(nomeConcat, "BESR%d.txt", n);
+//      if (SD.exists(nomeConcat))
+//        n++;
+//      else
+//        arquivoLog = SD.open(nomeConcat, FILE_WRITE);
+//        parar = true;
+//        novoarquivo = 1;
+//    }
+//
+//
+//  }
 }
